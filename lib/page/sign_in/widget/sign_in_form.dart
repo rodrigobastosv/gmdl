156475ff529a3047185884ba8041b962d7dd9679
rodrigobastosv <rodrigobastosv@gmail.com>
2021-01-21 11:25:17 -0300

    subindo elementos sign in

--- lib/page/sign_in/widget/sign_in_form.dart
+++ lib/page/sign_in/widget/sign_in_form.dart
@@ -21,98 +21,99 @@ class _SignInFormState extends State<SignInForm> {
   @override
   Widget build(BuildContext context) {
     return Form(
-        key: _formKey,
-        child: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              Image.asset('assets/images/banner_gm.png'),
-              const SizedBox(height: 140),
-              Padding(
-                padding: const EdgeInsets.all(18),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        Expanded(
-                          child: TextFormField(
-                            decoration: const InputDecoration(
-                              border: OutlineInputBorder(),
-                              hintText: 'Username',
-                              fillColor: Colors.white,
-                              filled: true,
-                              prefixIcon: Icon(Icons.person_pin),
-                            ),
-                            onSaved: (username) => _username = username,
-                            validator: (username) =>
-                                username.isEmpty ? 'Required Field' : null,
+      key: _formKey,
+      child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Image.asset('assets/images/banner_gm.png'),
+            const SizedBox(height: 80),
+            Padding(
+              padding: const EdgeInsets.all(18),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Username',
+                            fillColor: Colors.white,
+                            filled: true,
+                            prefixIcon: Icon(Icons.person_pin),
                           ),
+                          onSaved: (username) => _username = username,
+                          validator: (username) =>
+                              username.isEmpty ? 'Required Field' : null,
                         ),
-                        const SizedBox(width: 6),
-                        SvgPicture.asset(
-                          'assets/icons/qrcode.svg',
-                          height: 26,
-                          width: 26,
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 12),
-                    Row(
-                      children: <Widget>[
-                        Expanded(
-                          child: TextFormField(
-                            obscureText: true,
-                            decoration: const InputDecoration(
-                              border: OutlineInputBorder(),
-                              hintText: 'Password',
-                              fillColor: Colors.white,
-                              filled: true,
-                              prefixIcon: Icon(Icons.lock),
-                            ),
-                            onSaved: (password) => _password = password,
-                            validator: (password) =>
-                                password.isEmpty ? 'Required Field' : null,
+                      ),
+                      const SizedBox(width: 6),
+                      SvgPicture.asset(
+                        'assets/icons/qrcode.svg',
+                        height: 26,
+                        width: 26,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 12),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: TextFormField(
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Password',
+                            fillColor: Colors.white,
+                            filled: true,
+                            prefixIcon: Icon(Icons.lock),
                           ),
+                          onSaved: (password) => _password = password,
+                          validator: (password) =>
+                              password.isEmpty ? 'Required Field' : null,
                         ),
-                        const SizedBox(width: 6),
-                        SvgPicture.asset(
-                          'assets/icons/view.svg',
-                          color: Colors.white,
-                          height: 18,
-                          width: 18,
+                      ),
+                      const SizedBox(width: 6),
+                      SvgPicture.asset(
+                        'assets/icons/view.svg',
+                        color: Colors.white,
+                        height: 18,
+                        width: 18,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: RaisedButton(
+                          color: Theme.of(context).primaryColor,
+                          child: const Text(
+                            'Sign In',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                          onPressed: () {
+                            final _form = _formKey.currentState;
+                            if (_form.validate()) {
+                              _form.save();
+                              context.read<SignInCubit>().signInUser(
+                                    username: _username,
+                                    password: _password,
+                                  );
+                            }
+                          },
                         ),
-                      ],
-                    ),
-                    const SizedBox(height: 20),
-                    Row(
-                      children: <Widget>[
-                        Expanded(
-                          child: RaisedButton(
-                              color: Theme.of(context).primaryColor,
-                              child: const Text(
-                                'Sign In',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                ),
-                              ),
-                              onPressed: () {
-                                final _form = _formKey.currentState;
-                                if (_form.validate()) {
-                                  _form.save();
-                                  context.read<SignInCubit>().signInUser(
-                                        username: _username,
-                                        password: _password,
-                                      );
-                                }
-                              }),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
+                      ),
+                    ],
+                  )
+                ],
               ),
-            ],
-          ),
-        ));
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
