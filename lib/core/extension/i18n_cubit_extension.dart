c9ceecc2298f5ea1022313580f53c28dd5c51ea7
rodrigobastosv <rodrigobastosv@gmail.com>
2021-01-22 14:22:26 -0300

    adjust

--- lib/core/extension/i18n_cubit_extension.dart
+++ lib/core/extension/i18n_cubit_extension.dart
@@ -5,7 +5,7 @@ import 'package:provider/provider.dart';
 import '../cubit/cubits.dart';
 
 extension I18nCubitExt on BuildContext {
-  String getText(String id) => watch<I18nCubit>().getFormattedText(id);
+  String getText(String id) => read<I18nCubit>().getFormattedText(id);
 
   String getTextUppercase(String id) => getText(id).toUpperCase();
 }
