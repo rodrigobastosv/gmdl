3b082eac96492cda7819960f8d0e82654684d33a
rodrigobastosv <rodrigobastosv@gmail.com>
2021-02-02 09:02:58 -0300

    routing improves

--- lib/core/route/gm_router.dart
+++ lib/core/route/gm_router.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
+import '../utils/platform_utils.dart';
 import 'package:hive/hive.dart';
 
 import '../../page/pages.dart';
@@ -22,181 +24,163 @@ class GMRouter {
     final args = settings.arguments;
     final routeName = settings.name;
 
+    Widget routeWidget;
     switch (routeName) {
       case INITIAL_SETTINGS_PAGE:
-        return MaterialPageRoute(
-          builder: (context) => BlocProvider<InitialSettingsCubit>(
-            create: (_) => InitialSettingsCubit(
-              repository: InitialSettingsRepository(
-                GMClient(getBasicClient()),
-              ),
-              globalBox: Hive.box(GLOBAL_BOX),
+        routeWidget = BlocProvider<InitialSettingsCubit>(
+          create: (_) => InitialSettingsCubit(
+            repository: InitialSettingsRepository(
+              GMClient(getBasicClient()),
             ),
-            child: InitialSettingsPage(),
+            globalBox: Hive.box(GLOBAL_BOX),
           ),
-          settings: settings,
+          child: InitialSettingsPage(),
         );
+        break;
       case LOAD_INFO_PAGE:
         final username = args as String;
-        return MaterialPageRoute(
-          builder: (context) => BlocProvider(
-            create: (_) => LoadInfoCubit(
-              repository: context.read<LoadingInfoRepository>(),
-              globalInfo: context.read<GlobalInfo>(),
-              i18nCubit: context.read<I18nCubit>(),
-              deviceInfoService: G<DeviceInfoService>(),
-              packageInfoService: G<PackageInfoService>(),
-            )..getDriverInfo(username),
-            child: const LoadInfoPage(),
-          ),
-          settings: settings,
+        routeWidget = BlocProvider(
+          create: (context) => LoadInfoCubit(
+            repository: context.read<LoadingInfoRepository>(),
+            globalInfo: context.read<GlobalInfo>(),
+            i18nCubit: context.read<I18nCubit>(),
+            deviceInfoService: G<DeviceInfoService>(),
+            packageInfoService: G<PackageInfoService>(),
+          )..getDriverInfo(username),
+          child: const LoadInfoPage(),
         );
+        break;
       case CHOOSE_EQUIPMENT_PAGE:
-        return MaterialPageRoute(
-          builder: (context) => BlocProvider<ChooseEquipmentCubit>(
-            create: (_) => ChooseEquipmentCubit(
-              context.read<EquipmentRepository>(),
-            ),
-            child: ChooseEquipmentPage(),
+        routeWidget = BlocProvider<ChooseEquipmentCubit>(
+          create: (context) => ChooseEquipmentCubit(
+            context.read<EquipmentRepository>(),
           ),
-          settings: settings,
+          child: ChooseEquipmentPage(),
         );
+        break;
       case LOAD_ROUTE_PAGE:
-        return MaterialPageRoute(
-          builder: (context) => BlocProvider<LoadRouteCubit>(
-            create: (_) => LoadRouteCubit(
-              repository: context.read<RouteRepository>(),
-              globalInfo: context.read<GlobalInfo>(),
-              i18nCubit: context.read<I18nCubit>(),
-            )..fetchRouteInformation(),
-            child: const LoadRoutePage(),
-          ),
-          settings: settings,
+        routeWidget = BlocProvider<LoadRouteCubit>(
+          create: (context) => LoadRouteCubit(
+            repository: context.read<RouteRepository>(),
+            globalInfo: context.read<GlobalInfo>(),
+            i18nCubit: context.read<I18nCubit>(),
+          )..fetchRouteInformation(),
+          child: const LoadRoutePage(),
         );
+        break;
       case ROUTE_AT_GLANCE_PAGE:
         final route = args as RouteModel;
-        return MaterialPageRoute(
-          builder: (context) => BlocProvider<RouteCubit>(
-            create: (_) => RouteCubit(
-              route: route,
-              repository: context.read<RouteRepository>(),
-              globalInfo: context.read<GlobalInfo>(),
-              notificationCubit: context.read<NotificationCubit>(),
-            )..listenNotifications(),
-            child: const RouteAtGlancePage(),
-          ),
-          settings: settings,
+        routeWidget = BlocProvider<RouteCubit>(
+          create: (context) => RouteCubit(
+            route: route,
+            repository: context.read<RouteRepository>(),
+            globalInfo: context.read<GlobalInfo>(),
+            notificationCubit: context.read<NotificationCubit>(),
+          )..listenNotifications(),
+          child: const RouteAtGlancePage(),
         );
+        break;
       case ROUTE_SUMMARY_PAGE:
         final routeCubit = args as RouteCubit;
-        return MaterialPageRoute(
-          builder: (context) => BlocProvider.value(
-            value: routeCubit,
-            child: const RouteSummaryPage(),
-          ),
-          settings: settings,
+        routeWidget = BlocProvider.value(
+          value: routeCubit,
+          child: const RouteSummaryPage(),
         );
+        break;
       case SIGN_IN_PAGE:
-        return MaterialPageRoute(
-          builder: (context) => BlocProvider(
-            create: (_) => SignInCubit(
-              repository: context.read<SignInRepository>(),
-              globalBox: Hive.box(GLOBAL_BOX),
-            ),
-            child: SignInPage(),
+        routeWidget = BlocProvider(
+          create: (context) => SignInCubit(
+            repository: context.read<SignInRepository>(),
+            globalBox: Hive.box(GLOBAL_BOX),
           ),
-          settings: settings,
+          child: SignInPage(),
         );
+        break;
       case STOP_PAGE:
         final stopPageArguments = args as StopPageArguments;
-        return MaterialPageRoute(
-          builder: (context) => MultiBlocProvider(
-            providers: [
-              BlocProvider(
-                create: (_) => StopCubit(
-                  stop: stopPageArguments.stop,
-                  routeCubit: stopPageArguments.routeCubit,
-                  repository: context.read<StopRepository>(),
-                  globalInfo: context.read<GlobalInfo>(),
-                ),
+        routeWidget = MultiBlocProvider(
+          providers: [
+            BlocProvider(
+              create: (context) => StopCubit(
+                stop: stopPageArguments.stop,
+                routeCubit: stopPageArguments.routeCubit,
+                repository: context.read<StopRepository>(),
+                globalInfo: context.read<GlobalInfo>(),
               ),
-              BlocProvider.value(value: stopPageArguments.routeCubit),
-            ],
-            child: const StopPage(),
-          ),
-          settings: settings,
+            ),
+            BlocProvider.value(value: stopPageArguments.routeCubit),
+          ],
+          child: const StopPage(),
         );
+        break;
       case CHOOSE_CANCEL_CODE_PAGE:
         final stopCubit = args as StopCubit;
-        return MaterialPageRoute(
-          builder: (context) => MultiBlocProvider(
-            providers: [
-              BlocProvider.value(value: stopCubit),
-              BlocProvider(
-                create: (_) => ChooseCancelCodeCubit(),
-              ),
-            ],
-            child: const ChooseCancelCodePage(),
-          ),
-          settings: settings,
+        routeWidget = MultiBlocProvider(
+          providers: [
+            BlocProvider.value(value: stopCubit),
+            BlocProvider(
+              create: (_) => ChooseCancelCodeCubit(),
+            ),
+          ],
+          child: const ChooseCancelCodePage(),
         );
+        break;
       case CHOOSE_REDELIVER_CODE_PAGE:
         final stopCubit = args as StopCubit;
-        return MaterialPageRoute(
-          builder: (context) => MultiBlocProvider(
-            providers: [
-              BlocProvider.value(value: stopCubit),
-              BlocProvider(
-                create: (_) => ChooseRedeliverCodeCubit(),
-              ),
-            ],
-            child: const ChooseRedeliverCodePage(),
-          ),
-          settings: settings,
+        routeWidget = MultiBlocProvider(
+          providers: [
+            BlocProvider.value(value: stopCubit),
+            BlocProvider(
+              create: (_) => ChooseRedeliverCodeCubit(),
+            ),
+          ],
+          child: const ChooseRedeliverCodePage(),
         );
+        break;
       case CHOOSE_UNDELIVERABLE_CODE_PAGE:
         final stopCubit = args as StopCubit;
-        return MaterialPageRoute(
-          builder: (context) => MultiBlocProvider(
-            providers: [
-              BlocProvider.value(value: stopCubit),
-              BlocProvider(
-                create: (_) => ChooseUndeliverableCodeCubit(),
-              ),
-            ],
-            child: const ChooseUndeliverableCodePage(),
-          ),
-          settings: settings,
+        routeWidget = MultiBlocProvider(
+          providers: [
+            BlocProvider.value(value: stopCubit),
+            BlocProvider(
+              create: (_) => ChooseUndeliverableCodeCubit(),
+            ),
+          ],
+          child: const ChooseUndeliverableCodePage(),
         );
+        break;
       case STOP_LIST_PAGE:
         final routeCubit = args as RouteCubit;
-        return MaterialPageRoute(
-          builder: (context) => BlocProvider.value(
-            value: routeCubit,
-            child: const StopListPage(),
-          ),
-          settings: settings,
+        routeWidget = BlocProvider.value(
+          value: routeCubit,
+          child: const StopListPage(),
         );
+        break;
       case FINISHED_STOPS_PAGE:
         final routeCubit = args as RouteCubit;
         final stops = routeCubit.route.stops;
-        return MaterialPageRoute(
-          builder: (context) => BlocProvider.value(
-            value: routeCubit,
-            child: FinishedStopsPage(
-              stops: stops,
-            ),
+        routeWidget = BlocProvider.value(
+          value: routeCubit,
+          child: FinishedStopsPage(
+            stops: stops,
           ),
-          settings: settings,
         );
+        break;
       default:
-        return MaterialPageRoute(
-          builder: (_) => Scaffold(
-            body: Center(
-              child: Text('No route defined for ${settings.name}'),
-            ),
+        routeWidget = Scaffold(
+          body: Center(
+            child: Text('No route defined for ${settings.name}'),
           ),
         );
     }
+    return isAndroid()
+        ? MaterialPageRoute(
+            builder: (context) => routeWidget,
+            settings: settings,
+          )
+        : CupertinoPageRoute(
+            builder: (context) => routeWidget,
+            settings: settings,
+          );
   }
 }
